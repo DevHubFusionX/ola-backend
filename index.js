bfb50675cde0b95cb1d59643966bffcface5404a
Franklin Fanya <franklinfanya@gmail.com>
2026-02-28 19:08:49 +0100

    fix: add PATCH to CORS allowed methods

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ connectDB();
 app.use(cors({
   origin: ['https://olaluxe.vercel.app', 'http://localhost:3000', 'http://localhost:5173'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar']
 }));
